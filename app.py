d132bc350638b866f0815d6691c2b5f58c35b3d0
SabistaShaikh <138232986+SabistaShaikh@users.noreply.github.com>
2026-06-01 11:58:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
-import pandas as pd
 import joblib
 
-st.title("Credit Risk Intelligence")
-
-st.success("All packages loaded successfully")
-st.write("Pandas Version:", pd.__version__)
+st.title("Test")
+st.success("joblib imported successfully")
